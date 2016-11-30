391488bf2ce69f4f9ada9093cfd61a4ade974472
Melo0dy <5215ayaty@sina.com>
2016-11-30 21:17:32 +0800

    201511060167作业提交

---
+++ 4-作业提交/安滢_差别阈限/grey.m
@@ -0,0 +1,33 @@
+clc;clear;
+P = 'D:\YING\实心实验\201511060167安滢\A.BMP';
+a = imread(P);
+b=imadjust(a,[0 0.95],[]); %显示后再保存图片，大小发生变化
+imwrite(b,'D:\YING\实心实验\201511060167安滢\1.BMP');%输出为ri.jpg
+c=imadjust(a,[0 0.9],[]); %显示后再保存图片，大小发生变化
+imwrite(c,'D:\YING\实心实验\201511060167安滢\2.BMP');%输出为ri.jpg
+d=imadjust(a,[0 0.85],[]); %显示后再保存图片，大小发生变化
+imwrite(d,'D:\YING\实心实验\201511060167安滢\3.BMP');%输出为ri.jpg
+e=imadjust(a,[0 0.8],[]); %显示后再保存图片，大小发生变化
+imwrite(e,'D:\YING\实心实验\201511060167安滢\4.BMP');%输出为ri.jpg
+f=imadjust(a,[0 0.75],[]); %显示后再保存图片，大小发生变化
+imwrite(f,'D:\YING\实心实验\201511060167安滢\5.BMP');%输出为ri.jpg
+g=imadjust(a,[0 0.7],[]); %显示后再保存图片，大小发生变化
+imwrite(g,'D:\YING\实心实验\201511060167安滢\6.BMP');%输出为ri.jpg
+h=imadjust(a,[0 0.65],[]); %显示后再保存图片，大小发生变化
+imwrite(h,'D:\YING\实心实验\201511060167安滢\7.BMP');%输出为ri.jpg
+i=imadjust(a,[0 0.6],[]); %显示后再保存图片，大小发生变化
+imwrite(i,'D:\YING\实心实验\201511060167安滢\8.BMP');%输出为ri.jpg
+j=imadjust(a,[0 0.65],[]); %显示后再保存图片，大小发生变化
+imwrite(j,'D:\YING\实心实验\201511060167安滢\9.BMP');%输出为ri.jpg
+k=imadjust(a,[0 0.6],[]); %显示后再保存图片，大小发生变化
+imwrite(k,'D:\YING\实心实验\201511060167安滢\10.BMP');%输出为ri.jpg
+l=imadjust(a,[0 0.55],[]); %显示后再保存图片，大小发生变化
+imwrite(l,'D:\YING\实心实验\201511060167安滢\11.BMP');%输出为ri.jpg
+m=imadjust(a,[0 0.5],[]); %显示后再保存图片，大小发生变化
+imwrite(m,'D:\YING\实心实验\201511060167安滢\12.BMP');%输出为ri.jpg
+n=imadjust(a,[0.35 0.65],[]); %显示后再保存图片，大小发生变化
+imwrite(g,'D:\YING\实心实验\201511060167安滢\13.BMP');%输出为ri.jpg
+o=imadjust(a,[0.25 0.65],[]); %显示后再保存图片，大小发生变化
+imwrite(o,'D:\YING\实心实验\201511060167安滢\14.BMP');%输出为ri.jpg
+p=imadjust(a,[0.15 0.65],[]); %显示后再保存图片，大小发生变化
+imwrite(p,'D:\YING\实心实验\201511060167安滢\15.BMP');%输出为ri.jpg
